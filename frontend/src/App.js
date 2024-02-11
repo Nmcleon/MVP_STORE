2609664f2160721f85594c7c78b4692a006b5c0b
Nmcleon <nmcleon10@gmail.com>
2024-02-11 21:26:01 +0200

    Create products
    Add dummy data to render in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
+import { Container } from 'react-bootstrap'
 import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 
 const App = () => {
   return (
-    <div>Lore Fine Wine</div>
+    <>
+    <Header />
+    <main className='py-3'>
+      <Container>
+        <HomeScreen />
+    </Container>
+    </main>
+    <Footer />
+    </>
   )
 }
 
