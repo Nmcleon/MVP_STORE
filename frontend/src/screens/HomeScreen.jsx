b875711421ddb18f1c0c546921196f9d0513ce53
Nmcleon <nmcleon10@gmail.com>
2024-02-12 00:48:07 +0200

    Implemening Fetch
    Implemet fetch, get form dummy data next, start C in CRUD

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,9 +1,21 @@
+import { useEffect, useState } from 'react';
+import {Row, Col} from 'react-bootstrap';
 import React from 'react'
-import { Row, Col } from 'react-bootstrap'
+import axios  from "axios";
 import Product from '../components/Product'
-import products from '../products'
+
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const { data } = await axios.get('/api/products')
+            setProducts(data)
+        }
+        fetchProducts()
+    }, [])
+
   return (
     <>
         <h3>Latest Products</h3>
