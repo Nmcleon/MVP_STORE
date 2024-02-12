f9d599fb5190a29c1448b48d9f22ebe49c8e0382
Nmcleon <nmcleon10@gmail.com>
2024-02-12 11:16:59 +0200

    create data models
    useing mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import dotenv  from 'dotenv';
 dotenv.config();
+import connectDB from './config/db.js';
 // server.js
 import products from './data/products.js';
 
 
 const port = process.env.PORT;
+connectDB();
 const app = express();
 
 app.get('/', (req, res) => {
