417b199b9b020ea9d0215d1daca54c699ec93466
Nmcleon <nmcleon10@gmail.com>
2024-02-13 09:09:04 +0200

    Auth API slice

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import dotenv  from 'dotenv';
-import productRoutes from './routes/productRoutes.js'
 dotenv.config();
+import cookieParser from 'cookie-parser';
+import productRoutes from './routes/productRoutes.js'
+
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import userRoutes from './routes/userRoutes.js'
@@ -14,6 +16,9 @@ const app = express();
 // Body parser middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+// Cookie parser middleware
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
   res.send('API RUNNING...');
