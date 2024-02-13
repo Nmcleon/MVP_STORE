044bff0ddf0e17d0128a45ee2615aa081c9f6436
Nmcleon <nmcleon10@gmail.com>
2024-02-13 13:14:02 +0200

    Add screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -23,6 +23,7 @@ import RegisterScreen from './screens/RegisterScreen';
 import ShippingScreen from './screens/ShippingScreen';
 import PaymentScreen from './screens/PaymentScreens';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import OrderScreen from './screens/OrderScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -38,6 +39,8 @@ const router = createBrowserRouter(
         <Route path='/shipping' element={<ShippingScreen />} />
         <Route path='/payment' element={<PaymentScreen />} />
         <Route path='/placeorder' element={<PlaceOrderScreen />} />
+        <Route path='/order/:id' element={<OrderScreen />} />
+
 
       </Route>
     </Route>
